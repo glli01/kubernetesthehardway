3d85c3af2592334833e20561817ace0bf8496857
George Li <glli@ucsd.edu>
2025-05-09 22:04:52 -0500

    [APP] finish parts of 03-compute-resources

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,24 @@
+# 9 May, 2025
+Created machines.txt, made all machines sshable from root - NOTE, this required us to remove some lines from /root/.ssh/authorized_keys (The lines until `ssh-rsa`).
+
+Created a public key on jump-box. ssh-copy-id did not work, so manually added as a public key on each host.
+
+Changed hostnames on debian using `hostname` command.
+
+Locales changed using: `sudo dpkg-reconfigure locales` - select the en_US.UTF-8 option with space and tab and arrow keys. Continue and set that as default.
+
+Currently On:
+[Host Lookup Table](https://github.com/kelseyhightower/kubernetes-the-hard-way/blob/master/docs/03-compute-resources.md#host-lookup-table)
+
+# 4 May, 2025
+Create node-0, node-1, and server.
+Finished section 01 and 02, creating jump-box and setting it up.
+
+Currently on:
+[Compute Resources](https://github.com/kelseyhightower/kubernetes-the-hard-way/blob/master/docs/03-compute-resources.md)
 # 02/3 May, 2025
 Setup environment through terraform:
-1. VPC + Security groups
+1. VPC + Security groups + Internet gateway + Route Table
 2. Access key (aws_pub_key)
 3. Instances (Jump box)
+
--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -49,19 +49,31 @@ resource "aws_instance" "jump-box" {
   }
 }
 
-# Create VNIC to allow jump-box to connect to the internet
-# resource "aws_network_interface" "jump-box_vnic"{
-#   subnet_id = aws_subnet.resource[1].id
-#
-#   security_groups = [aws_security_group.allow_tls.id, aws_security_group.allow_ssh.id, aws_security_group.allow_http.id]
-#   tags = {
-#     Name = "jump-box_vnic"
-#   }
-# }
+# Create server (2GB RAM - 20GB Storage - 1 CPU) Create node-0 (2GB RAM - 20GB Storage - 1 CPU) Create node-1 (2GB RAM - 20GB Storage - 1 CPU)
+variable "instance_names" {
+  type = list(string)
+  default = ["server", "node-0", "node-1"]
+}
+resource "aws_instance" "server_and_nodes" {
+  count = length(var.instance_names)
+  ami           = var.debian_12_instance_ami
+  instance_type = "t2.small" # 2GB RAM
+  key_name      = aws_key_pair.kubernetes-the-hard-way-tf.key_name
 
-# Create server (2GB RAM - 20GB Storage - 1 CPU)
+  # Assign 10 GB of storage
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp3"
+  }
 
-# Create node-0 (2GB RAM - 20GB Storage - 1 CPU)
+  associate_public_ip_address = true
 
-# Create node-1 (2GB RAM - 20GB Storage - 1 CPU)
+  subnet_id = aws_subnet.resource[1].id
 
+  # Assign to security group and VPC
+  vpc_security_group_ids = [aws_security_group.allow_tls.id, aws_security_group.allow_ssh.id, aws_security_group.allow_http.id]
+
+  tags = {
+    Name = element(var.instance_names,count.index)
+  }
+}
